651ac9e2e69337c816923d95818dcb0166f2e28e
Jonah EG <jonah.edelmangold@gmail.com>
2025-04-21 20:24:56 -0500

    API get working

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Filter from './Filter.jsx'
 
 function App() {
 
   return (
     <>
+    <Filter></Filter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Filter.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function Filter(){
+//Create a filter for the relevent details
+    //Includes:
+        //Searchbar
+        //Colors
+        //Random button
+//Have the filter influence the URL
+//Then display the cards returned
+
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let list = JSON.parse(this.responseText);
+            console.log(list.data[1]);
+        }
+    };
+    xhttp.open("GET", "https://api.scryfall.com/cards/search?q=c%3Awhite+mv%3D1", true);
+    xhttp.send();
+    return(
+        <>
+        </>
+    )
+}
+
+export default Filter;
